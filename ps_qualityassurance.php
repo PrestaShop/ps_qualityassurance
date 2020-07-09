f4d40ae03385c70aced3595b48e25935f774a4ea
Pierre RAMBAUD <pierre.rambaud86@gmail.com>
2020-07-09 17:39:26 +0200

    Create table

--- ps_qualityassurance.php
+++ ps_qualityassurance.php
@@ -56,4 +56,17 @@ class Ps_Qualityassurance extends Module
 
         $this->assetsPath = $this->_path . 'views/dist/';
     }
+
+    public function install()
+    {
+        $query = 'CREATE TABLE IF NOT EXISTS `' . _DB_PREFIX_ . 'quality_assurance_hooks` (' .
+            '`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,' .
+            '`name` varchar(255) NOT NULL,' .
+            '`content` text NOT NULL,' .
+            'PRIMARY KEY (`id`),' .
+            'UNIQUE KEY `name` (`name`)' .
+            ') ENGINE=' . _MYSQL_ENGINE_ . ' DEFAULT CHARSET=UTF8;';
+
+        return Db::getInstance()->execute($query) && parent::install();
+    }
 }
